a3cf35268b38b051468cf64dc3874a80ef1aed16
joel <nextspam@web.de>
2018-10-13 15:25:38 +0800

    change structure and serve home.html with server

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
-const api = require('./api/routes');
-
+const path = require("path");
+const api = require("./api/routes");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,9 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", api);
 
 app.get("*", function(req, res){
-  console.log("todo: send index html");
-  res.send("soon you get tour page");
-  //res.sendFile()
+  res.sendFile(path.join(__dirname + "/frontend/home.html"));
 });
 
 app.listen(port, function () {
