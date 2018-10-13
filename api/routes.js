afe0d187fcb32066c98ba4127cd222f745ba27f7
joel <nextspam@web.de>
2018-10-13 17:35:16 +0800

    add sample data

--- api/routes.js
+++ api/routes.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var users = [{"username": "peter", "password": "mypass"}]
-var petAccomodations = []
+var petAccomodations = [
+  {"address": "A street 1", "description": "this is my house and i take care of your pet as good as i can"},
+  {"address": "B street 2", "description": "this is my house and i take care of your pet as good as i can"},
+  {"address": "C street 3", "description": "this is my house and i take care of your pet as good as i can"},
+  {"address": "D street 4", "description": "this is my house and i take care of your pet as good as i can"},
+  {"address": "E street 5", "description": "this is my house and i take care of your pet as good as i can"}
+]
 
 // LOGIN FUNCTIONALITY
 router.post("/login", function(req, res) {
@@ -27,10 +33,11 @@ router.post("/login", function(req, res) {
   res.end();
 });
 
-//REGIS
+//REGISTER
 router.post("/register", function(req, res){
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
 
 
   if(!isValidUsernamePass(username, password)){
@@ -46,6 +53,8 @@ router.post("/register", function(req, res){
   res.json({"status": "register success", "username": username});
 });
 
+
+//CREATE NEW ACCOMODATION
 router.post("/create", function(req, res){
   const address = req.body.address;
   const description = req.body.description;
@@ -59,10 +68,14 @@ router.post("/create", function(req, res){
   res.json({"status": "status success"});
 });
 
+
+//BOOK AN ACCOMODATION FOR YOUR PET
 router.post("/book", function(req, res){
 
 });
 
+
+//FETCH ALL POSSIBLE ACCOMODATIONS
 router.post("/fetch", function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.json({"data": petAccomodations});
